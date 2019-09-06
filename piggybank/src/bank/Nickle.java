65b086ced7ac65357a1684c8bc16f38b21e74c7c
deejayeaster@live.com <deejayeaster@live.com>
2019-09-06 12:34:38 -0400

    added nickle

--- piggybank/src/bank/Nickle.java
+++ piggybank/src/bank/Nickle.java
@@ -1,4 +1,12 @@
 package bank;
 
-public class Nickle {
+public class Nickle extends Money{
+
+    public Nickle(int quantity) {
+        super("Nickle", 0.05, quantity);
+    }
+
+    public Nickle() {
+        super("Nickle", 0.05, 1);
+    }
 }
